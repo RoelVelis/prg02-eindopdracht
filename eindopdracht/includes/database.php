034c6285e9e49b04261789edcb8ef3e4773d0d25
RoelVelis <145676613+RoelVelis@users.noreply.github.com>
2023-12-13 12:21:58 +0100

    Add files via upload

---
+++ eindopdracht/includes/database.php
@@ -0,0 +1,8 @@
+<?php
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "eindopdracht_prg2";
+
+$db = mysqli_connect($host, $user, $password, $database)
+or die("Error: " . mysqli_connect_error());;
